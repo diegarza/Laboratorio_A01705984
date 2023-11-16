401a3a43bc91fe26ceb1fc6c5f07dec6596f2dca
diegarza <98358978+diegarza@users.noreply.github.com>
2023-11-15 19:26:10 -0600

    Add files via upload

---
+++ AppDiego.py
@@ -0,0 +1,10 @@
+import datetime
+import time
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+st.title('Título: Analítica de Datos')
+
+st.header('Este es un header')
+st.subheader('Este es un subheader')
